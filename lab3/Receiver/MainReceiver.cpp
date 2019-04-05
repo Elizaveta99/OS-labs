09fba77d8b71c1279d6f8b35c1f4087b7b65e029
Elizaveta <eliz.vitebsk@gmail.com>
2019-04-05 09:16:38 +0300

    ok

--- lab3/Receiver/MainReceiver.cpp
+++ lab3/Receiver/MainReceiver.cpp
@@ -7,12 +7,11 @@
 
 using namespace std;
 
-HANDLE hInEvent, hWorkEvent;
+HANDLE hInEvent;
+fstream file;
 CHAR lpEventName[] = "ReadyForWork";
-CHAR lpWorkName[] = "CanWork";
 
-int FILE_SIZE = 60;
-const int MSG_AMOUNT = 3;
+const int MSG_AMOUNT = 3, MSG_SIZE = 20, FILE_SIZE = 60;
 
 int main()
 {
@@ -20,13 +19,14 @@ int main()
 	string filename;
 	cin >> filename; 
 
-	fstream file(filename, ios::out | ios::in | ios::binary);
+	ofstream file(filename, ios::out | ios::binary);
 
-	file.seekp(0, ios::beg); // сдвиг на 0 с начала
-	int beg_pos = 0, end_pos = 1 * sizeof(int), amount = 0; // pos 0 и 1 для указателей начала и конца очереди, amount - кол-во сообщений
+	file.seekp(0, ios::beg); 
+	int beg_pos = 3 * sizeof(int), end_pos = 3 * sizeof(int), amount = 0; 
 	file.write((char*)&amount, sizeof(int));
 	file.write((char*)&beg_pos, sizeof(int));
 	file.write((char*)&end_pos, sizeof(int));
+	file.close();
 
 	cout << "Sender's amount : "; 
 	int sender_amount; 
@@ -39,16 +39,13 @@ int main()
 	if (hMutex == NULL)
 	{
 		cout << "Create mutex failed." << endl;
-		cout << "Press any key to exit." << endl;
-		system("pause");
 		return GetLastError();
 	}
-
-	//  параметры?
+	
 	HANDLE hSemaphoreReceive = CreateSemaphore(NULL, 0, MSG_AMOUNT, "SemaphoreReceive");
 	if (hSemaphoreReceive == NULL)
 		return GetLastError();
-	HANDLE hSemaphoreSend = CreateSemaphore(NULL, 0, MSG_AMOUNT, "SemaphoreSend");
+	HANDLE hSemaphoreSend = CreateSemaphore(NULL, MSG_AMOUNT, MSG_AMOUNT, "SemaphoreSend"); 
 	if (hSemaphoreSend == NULL)
 		return GetLastError();
 
@@ -60,12 +57,18 @@ int main()
 		PROCESS_INFORMATION piSender;
 		ZeroMemory(&si, sizeof(STARTUPINFO));
 		si.cb = sizeof(STARTUPINFO);
-		string 	param = lpszAppName + " " + filename;
+		string 	param = lpszAppName + " " + filename + " " + lpEventName  + to_string(glob_i);
+		string name1 = lpEventName + to_string(glob_i);
+		char* name = const_cast<char*>(name1.c_str());
+
 		int destSize = param.size() + 1;
 		char* lpszCommandLine = const_cast<char*>(param.c_str());
 
-		//cout << "cmd = " << param << ' ' << lpszCommandLine << "\n";
-
+		// создание события для отправки Receiver сигнала на готовность к работе
+		hInEvent = CreateEvent(NULL, FALSE, FALSE, name);
+		if (hInEvent == NULL)
+			return GetLastError();
+		
 		if (!CreateProcess(NULL, lpszCommandLine, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &piSender))
 		{
 			cout << "The new process is not created.\n";
@@ -75,139 +78,86 @@ int main()
 			return dw;
 		}
 
-		// создание события для отправки Receiver сигнала на готовность к работе
-		hInEvent = CreateEvent(NULL, FALSE, FALSE, lpEventName);
-		if (hInEvent == NULL)
-		{
-			//system("pause");
-			//cout << GetLastError() << endl;
-			return GetLastError();
-		}
-
 		wait_array[event_amount] = hInEvent;
 		sender_array[event_amount++] = piSender;
-
-		// ждем оповещение о готовности от каждого? процесса Sender  ??
-		DWORD dwWaitResult = WaitForSingleObject(hInEvent, INFINITE); // 300 ?
-		if (dwWaitResult != WAIT_OBJECT_0)
-		{
-			cout << dwWaitResult << endl;
-			cout << "not ok wait\n";
-			system("pause");		
-			return dwWaitResult;
-		}
-		
-		//или дальше ?
-		/*WaitForSingleObject(piSender.hProcess, INFINITE);
-		CloseHandle(piSender.hThread);
-		CloseHandle(piSender.hProcess);*/
-
-		//CloseHandle(hInEvent); // или дальше
 	}
 
-	//if (WaitForMultipleObjects(sender_amount, wait_array, TRUE, INFINITE) == WAIT_FAILED)
-	//{
-	//	cout << "Wait for multiple objects failed." << endl;
-	//	cout << "Press any key to exit." << endl;
-	//	system("pause");
-	//	return 0; // ??
-	//}
+	if (WaitForMultipleObjects(sender_amount, wait_array, TRUE, INFINITE) == WAIT_FAILED)
+	{
+		cout << "Wait for multiple objects failed." << endl;
+		cout << "Press any key to exit ";
+		cin.ignore();
+		cin.get();
+		return 0; 
+	}
+	
+	// читаем сообщения
+	cout << "Press 1 to receive message or 2 to exit\n"; 
+	while (true)
+	{
+		int type;
+		cin >> type;
+		if (type == 1)
+		{
+			char mesg[MSG_SIZE];
 
-	for (int i = 0; i < sender_amount; i++) 
+			WaitForSingleObject(hSemaphoreReceive, INFINITE);
+			WaitForSingleObject(hMutex, INFINITE);
+
+			fstream file(filename, ios::out | ios::in | ios::binary);
+
+			int end_pos = -1, beg_pos = -1, amount = 0;
+			file.read((char*)&amount, sizeof(int));
+			file.read((char*)&beg_pos, sizeof(int));
+			file.read((char*)&end_pos, sizeof(int));
+				
+			if (amount == 0) // файл пустой
+			{
+				WaitForSingleObject(hSemaphoreSend, INFINITE);
+			}                       
+			else if (beg_pos >= FILE_SIZE + 3 * sizeof(int)) 
+				beg_pos = 3 * sizeof(int); 
+				
+				
+			file.seekg(beg_pos, ios::beg); // установила текущую позицию начала очереди
+			file.read((char*)&mesg, MSG_SIZE); // прочитала сообщение 
+
+			cout << "Message : " << mesg;
+
+			file.seekp(0, ios::beg);
+			amount--;
+			file.write((char*)&amount, sizeof(int));
+
+			beg_pos += MSG_SIZE;						// пересчитала новую позицию начала очереди
+			file.seekp(1 * sizeof(int), 0);				// на 1 месте хранится значение начала очереди
+			file.write((char*)&beg_pos, sizeof(int));	// записала новое значение начала очереди
+
+			file.close();
+			// освобождаем мьютекс
+			ReleaseMutex(hMutex);
+			// ++ произвели единицу ресурса
+			ReleaseSemaphore(hSemaphoreSend, 1, NULL); 
+		}
+		else
+			break;
+		cout << "Press type : \n";
+	}
+
+	for (int i = 0; i < sender_amount; i++)
 	{
 		CloseHandle(wait_array[i]);
-		WaitForSingleObject(sender_array[i].hProcess, INFINITE);
 		CloseHandle(sender_array[i].hThread);
 		CloseHandle(sender_array[i].hProcess);
 	}
 
-	delete[] wait_array;
-
-	
-
-		// ?? надо?
-		//hWorkEvent = CreateEvent(NULL, FALSE, TRUE, lpWorkName); //посылаю разрешение на роботу всех Sender : сразу в сигнальном состоянии
-		//SetEvent(hWorkEvent);
-		//if (hWorkEvent == NULL)
-		//{
-		//	cout << "error = " <<  GetLastError() << endl;
-		//	system("pause");
-		//	return GetLastError();
-		//}
-
-		
-
-		// читаем сообщения
-
-		cout << "Press 1 to receive message or 2 to exit\n"; // in 1 string or 2 ??
-		while (true)
-		{
-			int type;
-			cin >> type;
-			if (type == 1)
-			{
-				//string msg;
-				char* mesg;// = const_cast<char*>(msg.c_str()); // ?? надо ли?
-
-				// --
-				WaitForSingleObject(hSemaphoreReceive, INFINITE);
-
-				// захватываем мьютекс
-				WaitForSingleObject(hMutex, INFINITE);
-
-				file.open(filename, ios::binary | ios::in | ios::out);
-
-				int end_pos = -1, beg_pos = -1, amount = 0;
-				file.read((char*)&amount, sizeof(int));
-				file.read((char*)&beg_pos, sizeof(int));
-				file.read((char*)&end_pos, sizeof(int));
-
-				if (amount == 0)
-				{
-					if (beg_pos == end_pos) // файл пустой
-					{
-						WaitForSingleObject(hSemaphoreSend, INFINITE);
-						// wait ??
-						//  
-					}                       
-					else 
-					//{
-						end_pos = 2;
-					//}
-				}
-
-				file.seekg(0, end_pos); // +/- 1 ?? установила текущую позицию конца очереди
-				file.read((char*)&mesg, sizeof(mesg)); // записала сообщение
-				beg_pos += sizeof(mesg); // пересчитала новую позицию начала очереди
-				file.seekg(0, 0); // на 0 месте хранится значение начала очереди
-				file.write((char*)&beg_pos, sizeof(int)); // записала новое значение начала очереди
-
-				file.close();
-				// освобождаем мьютекс
-				ReleaseMutex(hMutex);
-				// ++ произвели единицу ресурса
-				ReleaseSemaphore(hSemaphoreSend, 1, NULL); // NULL??
-				//Sleep(500); // ??
-			}
-			else
-				break;
-		}
-
-		
-		//CloseHandle(hWorkEvent);
-
-	/*for (int i = 0; i < sender_amount; i++)
-	{
-		CloseHandle(piSender.hThread);
-		CloseHandle(piSender.hProcess);
-	}*/
-
+	CloseHandle(hMutex);
 	CloseHandle(hSemaphoreReceive);
 	CloseHandle(hSemaphoreSend);
 
 	cout << "Receiver works\n";
-	//cin.get(); // ??
-	system("pause");
+	cout << "Press any key to exit ";
+	cin.ignore();
+	cin.get();
 
 	return 0;
 }
--- lab3/Sender/MainSender.cpp
+++ lab3/Sender/MainSender.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <iostream>
 #include <fstream>
@@ -7,35 +9,31 @@
 
 using namespace std;
 
-HANDLE hInEvent, hWorkEvent;
+HANDLE hInEvent;
 CHAR lpEventName[] = "ReadyForWork"; 
-CHAR lpWorkName[] = "CanWork";
 HANDLE hSemaphore;
 
-int FILE_SIZE = 63;
-int MSG_AMOUNT = 3;
+int FILE_SIZE = 60;
+const int MSG_AMOUNT = 3, MSG_SIZE = 20;
 
-int main(int argv, char** args)
+int main(int args, char** argv)
 {
-	cout << "Sender !\n";
 	fstream file;
 
-	//fstream file(args[1], ios::in | ios::out | ios::binary); 
+	// открытие события для отправки Receiver сигнала на готовность к работе
+	hInEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, argv[2]);
+	if (hInEvent == NULL)
+	{
+		cout << "Open event failed." << endl;
+		cout << "Input any char to exit." << endl;
+		char c;
+		cin >> c;
+		return GetLastError();
+	}
 
 	SetEvent(hInEvent); // послали сигнал о своей готовности
-	Sleep(500); //??
-
-	//DWORD dwWaitResult = WaitForSingleObject(hWorkEvent, INFINITE); // ждём разрешения на продолжение работы, когда все Senders готовы
-
-//system("pause");
-//	return 0;
-
-	/*if (dwWaitResult != WAIT_OBJECT_0) 
-	{
-		cout << dwWaitResult << endl;
-		system("pause");
-		return dwWaitResult;
-	}*/
+	cout << "Sender \n";
+	CloseHandle(hInEvent);
 
 	HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, "FileMutex");
 	if (hMutex == NULL)
@@ -56,60 +54,58 @@ int main(int argv, char** args)
 		cin >> type;
 		if (type == 1)
 		{
-			cout << "Message : \n";
-			string msg;
-			cin >> msg;
-			char* mesg = const_cast<char*>(msg.c_str()); // ?? надо ли?
+			cout << "Message : \n";		
+			//string msg;
+			char mesg[MSG_SIZE];
+			cin >> mesg;
 
 			WaitForSingleObject(hSemaphoreSend, INFINITE);
 			WaitForSingleObject(hMutex, INFINITE);
 
-			file.open(args[1], ios::binary | ios::in | ios::out);
+			//cout << mesg << endl;
 
-			int end_pos = -1, beg_pos = -1, amount = 0;
+			file.open(argv[1], ios::binary | ios::in | ios::out);
+
+			int end_pos = -1, beg_pos = -1, amount = -1;
 			file.read((char*)&amount, sizeof(int));
 			file.read((char*)&beg_pos, sizeof(int));
 			file.read((char*)&end_pos, sizeof(int));
 
-			
+			//cout << "read : " << amount << ' ' << beg_pos << ' ' << end_pos << "\n";
+
 				if (amount == MSG_AMOUNT) // файл переполнен
 				{
 					WaitForSingleObject(hSemaphoreReceive, INFINITE);
-					// wait ??
-					// end_pos поменять 
 				}
-				else if (end_pos >= FILE_SIZE * sizeof(int))// в конец записать нельзя, но ещё место есть, зацикливаем
-				//{
-					end_pos = 3 * sizeof(int); // исправить везде в байтах ? 4 * sizeof ??
-				//}
+				else if (end_pos >= FILE_SIZE + 3 * sizeof(int))
+					end_pos = 3 * sizeof(int); 				
 			
-			
-			file.seekp(end_pos + amount * sizeof(mesg), ios::beg); // +/- 1 ?? получила текущую позицию конца очереди
-			file.write((char*)&mesg, sizeof(mesg)); // записала сообщение
+			file.seekp(end_pos, ios::beg);
+			//file.seekp(end_pos + amount * sizeof(mesg), ios::beg); // +/- 1 ?? получила текущую позицию конца очереди
+			file.write((char*)&mesg, MSG_SIZE); // записала сообщение
 
 			file.seekp(0, ios::beg);
 			amount++;
 			file.write((char*)&amount, sizeof(int));
 
-			end_pos += amount * sizeof(mesg); // пересчитала новую позицию конца очереди
-			file.seekp(sizeof(int), ios::beg); // на 1 месте хранится значение конца очереди
+			end_pos += MSG_SIZE; // пересчитала новую позицию конца очереди
+			file.seekp(2 * sizeof(int), ios::beg); // на 2 месте хранится значение конца очереди
 			file.write((char*)&end_pos, sizeof(int)); // записала новое значение конца очереди
 
+			file.close();
 			ReleaseMutex(hMutex);
-			ReleaseSemaphore(hSemaphoreReceive, 1, NULL); // ++ read
-			Sleep(500); // ??
+			ReleaseSemaphore(hSemaphoreReceive, 1, NULL); // ++ read			
 		}
 		else
 			break;
+		cout << "Press type : \n";
 	}
 
-	CloseHandle(hMutex);
-	CloseHandle(hSemaphoreReceive);
-	CloseHandle(hSemaphoreSend);
 	file.close();
 
-	cout << "Press any key to exit: ";
-	system("pause"); // изменить на get?
+	cout << "Press any key to exit ";
+	cin.ignore();
+	cin.get(); 
 
 	return 0;
 }
